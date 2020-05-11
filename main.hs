1696e1ba027c8d3738d8bd24a7962fcd9e46f8b7
Miller Bath <millerzbath@gmail.com>
2020-05-11 18:52:56 -0500

    Created data structure to hold game state, and init

--- main.hs
+++ main.hs
@@ -7,13 +7,13 @@ width = 800 -- actual window demension
 height = 600 -- actual window demension
 offset = 100 -- where does the window populate onto the computer screen
 
-window :: Display
+window :: Display -- window properties
 window = InWindow "Bouncing Betty" (width, height) (offset, offset)
 
-background :: Color
+background :: Color -- background properties
 background = black
 
-drawing :: Picture
+drawing :: Picture -- create the images for the game
 drawing = pictures [ball, walls,
                     mkPaddle green (280) 0 90]
   where
@@ -40,6 +40,28 @@ drawing = pictures [ball, walls,
       ]
 
     paddleColor = light (light white)
+
+-- current game value
+-- use game to type check the values passed into PongGame
+data PongGame = Game {
+  ballLocation :: (Float,Float),
+  ballVelocity :: (Float, Float)
+  player :: Float
+} deriving Show -- so we can read from this structure
+
+-- initial game values
+initState :: PongGame
+initState = Game {
+  ballLocation = (0,0)
+  ballVelocity = (-1, -1)
+  player = 40
+}
+
+-- take the PongGame and create a picture(static image) with it
+render :: PongGame -> Picture
+
+
+
 
 main :: IO ()
 main = display window background drawing 
