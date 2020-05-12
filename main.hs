cf0c26f1e206100d56fa7cf83cb4b845553b2703
Miller Bath <millerzbath@gmail.com>
2020-05-11 19:32:07 -0500

    Added function to calculate next ball position

--- main.hs
+++ main.hs
@@ -45,22 +45,64 @@ drawing = pictures [ball, walls,
 -- use game to type check the values passed into PongGame
 data PongGame = Game {
   ballLocation :: (Float,Float),
-  ballVelocity :: (Float, Float)
+  ballVelocity :: (Float, Float),
   player :: Float
 } deriving Show -- so we can read from this structure
 
 -- initial game values
 initState :: PongGame
 initState = Game {
-  ballLocation = (0,0)
-  ballVelocity = (-1, -1)
+  ballLocation = (0,0),
+  ballVelocity = (-1, -1),
   player = 40
 }
 
 -- take the PongGame and create a picture(static image) with it
+-- this function is to be used constantly to refresh the current game, ball, and player's pad
+-- ??? why do we take player next_game instead of next_game player ???
 render :: PongGame -> Picture
+render next_game = pictures [ball, walls, mkPaddle green 280 (player next_game) 90]
+  where
+    -- ball props
+    ball = uncurry translate (ballLocation next_game) $ color ballColor $ circleSolid 10
+    ballColor = dark red
+
+    --  Top and side walls.
+    wall :: Float -> Float -> Float -> Float -> Picture
+    wall x y rot length =
+      rotate rot $
+        translate x y $
+          color wallColor $
+            rectangleSolid length 10
+
+    wallColor = greyN 0.5
+    walls = pictures [wall 0 290 0 790, wall 0 390 90 590, wall 0 (-390) 90 590]
+
+    --  Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Float -> Picture
+    mkPaddle col x y rot = pictures
+      [ rotate rot $ translate x y $ color col $ rectangleSolid 26 106
+      , rotate rot $ translate x y $ color paddleColor $ rectangleSolid 20 100
+      ]
+
+    paddleColor = light (light white)
+
+moveBall :: Float -> PongGame -> PongGame
+position
+
+-- execute our where before we return the new ball location coords
+moveBall timePassed current_game = current_game { ballLocation = (x',y') }
+  where
+    (x, y) = ballLocation current_game
+    (xVol, yVol) = ballVelocity current_game
+    x' = x + xVol * timePassed
+    y' = y + yVol * timePassed
 
 
+
+
+animate :: Display -> Color -> (Float -> Picture) -> IO()
+animate = 
 
 
 main :: IO ()
